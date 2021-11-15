7cb3b69d6d6f728f482d08f4f9872dbfd293a692
Alexandre Marques Bulcao <58255516+ambulcao@users.noreply.github.com>
2021-11-15 20:01:44 +0000

    Adicionando script link

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMeSomething(){
+  alert(links.instagram)
+}
+
+const link = {
+  github: "jakeliny",
+  youtube: "jakelinygracielly",
+  facebook: "maykbrito",
+  instagram: "jackeliny.gracielly",
+  twitter: "jackelinytec"
+}
+
+showMeSomething();
